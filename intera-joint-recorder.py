3681b93f5f3ed01e823f9bcc46c8d6fb8c7efc4d
cloudy <im@joe.cloud>
2019-03-04 02:24:31 -0600

    REV: Minor clean up, remove prints

--- intera-joint-recorder.py
+++ intera-joint-recorder.py
@@ -14,27 +14,17 @@ cuff = intera_interface.Cuff('right')
 lights = intera_interface.Lights()
 
 # Move the robot to the starting point
-angles = limb.joint_angles()
-angles['right_j0'] = np.radians(0)
-angles['right_j1'] = np.radians(-50)
-angles['right_j2'] = np.radians(0)
-angles['right_j3'] = np.radians(120)
-angles['right_j4'] = np.radians(0)
-angles['right_j5'] = np.radians(0)
-angles['right_j6'] = np.radians(0)
+angles = dict(zip(limb.joint_names(), np.radians([0,-50,0,120,0,0,0])))
 limb.move_to_joint_positions(angles)
 
 lights.set_light_state('right_hand_red_light')
-
 while not cuff.lower_button():
     pass
 
-print('Recording of trajectory has started!')
 lights.set_light_state('right_hand_green_light')
 lights.set_light_state('right_hand_red_light', False)
 
-q = []
-t = []
+q, t = [], []
 while not cuff.lower_button():
     joint_positions = limb.joint_angles()
     q.append([float(joint_positions[i]) for i in limb.joint_names()])
@@ -45,12 +35,9 @@ lights.set_light_state('right_hand_red_light')
 lights.set_light_state('right_hand_green_light', False)
 
 # Save trajectory
-traj_final = np.concatenate((np.array(q), np.multiply(np.ones((len(q), 1)), 0.0402075604203)), axis=1)
-time = np.linspace(0, t[-1] - t[0], len(q)).reshape((len(q), 1))
-traj_final = np.concatenate((time, traj_final), axis=1)
+traj_final = np.concatenate((np.array(t).reshape((len(t),1))-t[0], np.array(q), np.ones((len(q),1)*0.0402), axis=1)
 
 # Save trajectory
-print('Saving trajectory as %s' % arg.output_file)
 header = 'time,right_j0,right_j1,right_j2,right_j3,right_j4,right_j5,right_j6,right_gripper'
 np.savetxt(arg.output_file, traj_final, delimiter=',', header=header, comments='', fmt="%1.12f")
 
