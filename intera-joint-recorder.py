dfefd0ce5cf22383edfc50f10f5ed02bddaa83e3
cloudy <im@joe.cloud>
2019-03-04 02:29:47 -0600

    Move argparser into single file, remove unused gripper object

--- intera-joint-recorder.py
+++ intera-joint-recorder.py
@@ -3,18 +3,20 @@
 import numpy as np
 import rospy
 import intera_interface
-from args import arg
+import argparse
+
 
 rospy.init_node("intera_joint_recorder")
 
-# Create an object to interface with the arm
+parser = argparse.ArgumentParser(description="Record joint trajectories")
+parser.add_argument('-of', '--output-file', type=str, default='output.txt')
+arg = parser.parse_args()
+
 limb = intera_interface.Limb('right')
-gripper = intera_interface.Gripper('right')
 cuff = intera_interface.Cuff('right')
 lights = intera_interface.Lights()
 
-# Move the robot to the starting point
-angles = dict(zip(limb.joint_names(), np.radians([0,-50,0,120,0,0,0])))
+angles = dict(zip(limb.joint_names(), np.radians([0,-50,0,120,0,0,0]))) # Start pose
 limb.move_to_joint_positions(angles)
 
 lights.set_light_state('right_hand_red_light')
@@ -34,10 +36,7 @@ while not cuff.lower_button():
 lights.set_light_state('right_hand_red_light')
 lights.set_light_state('right_hand_green_light', False)
 
-# Save trajectory
 traj_final = np.concatenate((np.array(t).reshape((len(t),1))-t[0], np.array(q), np.ones((len(q),1)*0.0402), axis=1)
-
-# Save trajectory
 header = 'time,right_j0,right_j1,right_j2,right_j3,right_j4,right_j5,right_j6,right_gripper'
 np.savetxt(arg.output_file, traj_final, delimiter=',', header=header, comments='', fmt="%1.12f")
 
